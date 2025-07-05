d39c79b84eafb6066a874743e683c00a80cb641f
CallMeCal01 <crjm04@gmail.com>
2025-07-05 22:16:59 +1200

    Update shopPage.js

--- scripts/shopPage.js
+++ scripts/shopPage.js
@@ -1,8 +1,5 @@
 
 
-var productsPath = "../shop/products/products.html";
-var pricesPath = "../shop/prices/prices.html";
-var stockPath = "../shop/products/stocklevel.html";
 
 var products = [
                 "Vanilla Soy Wax Candle",
@@ -10,7 +7,5 @@ var products = [
                 "Whiskey, Wood Smoke and Leather",
             ];
 
-var prices = document.getElementById(pricesPath).innerHTML;
-var stock = document.getElementById(stockPath).innerHTML;
 
-console.log(products, prices, stock);
+console.log(products);
