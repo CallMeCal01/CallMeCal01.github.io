a3fb52c6c95ff9f1bafe507990a795983c3ec626
CallMeCal01 <crjm04@gmail.com>
2025-07-05 22:09:01 +1200

    Update shopPage.js

--- scripts/shopPage.js
+++ scripts/shopPage.js
@@ -4,4 +4,4 @@ var productsPath = "../shop/products/products.html";
 var pricesPath = "../shop/prices/prices.html";
 var stockPath = "../shop/products/stocklevel.html";
 
-print(productsPath, pricesPath, stockPath);
+console.log(productsPath, pricesPath, stockPath);
